e04f43a895d593d5f5bb1454c738f42188d94c17
Ingo Fischer <github@fischer-ka.de>
2017-09-02 22:07:36 +0200

    update some files and try to get testing working

---
+++ build/lib/utils.d.ts
@@ -0,0 +1,7 @@
+declare const _default: {
+    readonly controllerDir: string;
+    getConfig(): string;
+    adapter(adapterName: string): ioBroker.Adapter;
+    adapter(adapterOptions: ioBroker.AdapterOptions): ioBroker.Adapter;
+};
+export default _default;
