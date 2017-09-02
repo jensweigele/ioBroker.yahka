bb293be7a464173ab9977d134566bd82b0b4190f
Ingo Fischer <github@fischer-ka.de>
2017-09-02 22:14:27 +0200

    Revert "update some files and try to get testing working"
    This reverts commit e04f43a895d593d5f5bb1454c738f42188d94c17.

--- build/lib/utils.d.ts
+++
@@ -1,7 +0,0 @@
-declare const _default: {
-    readonly controllerDir: string;
-    getConfig(): string;
-    adapter(adapterName: string): ioBroker.Adapter;
-    adapter(adapterOptions: ioBroker.AdapterOptions): ioBroker.Adapter;
-};
-export default _default;
