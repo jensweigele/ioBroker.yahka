da0f810c498b180afe552551dc443491bea5f98b
jw <jensweigele@googlemail.com>
2019-01-03 12:00:26 +0100

    Cleaned gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -12,39 +12,15 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: pkg,
         copy: {
-            build: {
-                // files: [
-                //     { src: 'dist/main.js', dest: "main.js"},
-                //     { src: 'dist/yahka.admin.js', dest: "admin/yahka.admin.js"},
-                    
-                // ]
-            },
             deployTestInstance: {
                 files: [
                     // copy files to build directory
                     { expand: true, cwd: '', src: ['**', '!src/**', '!node_modules/**'], dest: destinationDir + 'node_modules/iobroker.yahka/' }
                 ]
-
             }
         },
-
-        clean: {
-                build: [] // ['yahka.*.js', 'main.js', 'admin/yahka.*.js']
-        },
-
-        // ts: {
-        //     build: {
-        //         tsconfig: true
-        //     }
-        // },
         webpack: {
             webpackConfig
-            // options: {
-            //   stats: !process.env.NODE_ENV || process.env.NODE_ENV === 'development',
-            //   mode: "development"
-            // },
-            // prod: ,
-            // dev: Object.assign({ watch: true }, webpackConfig)
           },
 
         exec: {
@@ -64,22 +40,12 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    //grunt.loadNpmTasks('grunt-ts');
     grunt.loadNpmTasks('grunt-webpack');
     grunt.loadNpmTasks('grunt-exec');
 
     grunt.registerTask('buildOnly', [
-        'clean:build',
         'webpack',
-        'replace',
-        'copy:build'
     ]);
-    grunt.registerTask('replace', function () {
-        // var file = require('fs').readFileSync('./admin/yahka.admin.js');
-        // file = file.toString().replace('Object.defineProperty(exports, "__esModule", { value: true });', '').replace('var $ = require("jquery");', '');
-        // require('fs').writeFileSync('./admin/yahka.admin.js', file);   
-    });
     
     grunt.registerTask('DeployToTest', [
         'copy:deployTestInstance',
@@ -87,27 +53,18 @@ module.exports = function(grunt) {
     ]);
 
     grunt.registerTask('BuildAndDeployToTest', [
-        'clean:build',
         'webpack',
-        'replace',
-        'copy:build',
         'copy:deployTestInstance',
         'exec:refreshIOBroker'
     ]);
 
     grunt.registerTask('NPMPublish', [
-        'clean:build',
         'webpack',
-        'replace',
-        'copy:build',
         'exec:NPMPublish'        
     ])
 
     grunt.registerTask('NPMBeta', [
-        'clean:build',
         'webpack',
-        'replace',
-        'copy:build',
         'exec:NPMBeta'        
     ])
 };
